dee4ac4440469c55730d47f12bf71316aac63de1
baspovich <65213304+baspovich@users.noreply.github.com>
2021-06-11 23:00:51 +0800

    add README.md

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,8 @@ import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('init javascript')
-    const audio = new Audio('https://myradio24.org/vinila'),
+    console.log('init javascript') // инициализация скрипта 
+    const audio = new Audio('https://myradio24.org/vinila'), // ссылка на поток в 320 кбит
           player = document.querySelector('.player'),
           screen = player.querySelector('.player__live'),
           playBtn = player.querySelector('.player__nav-play'),
@@ -32,24 +32,24 @@ window.addEventListener('DOMContentLoaded', () => {
           radiator = player.querySelector('.player__quality'),
           playBtnSlider = document.querySelector('#radio-play-btn');
 
-    let seconds = 0,
+    let seconds = 0, // ГЛОБАЛЬНЫЕ переменные, который использует таймер
         minutes = 0,
         hours = 0;
 
     let durTimer,
-        volumeStat = 50;
+        volumeStat = 50; // ГЛОБАЛЬНЫЕ переменные, которые используются для сохранения информации о настройках эквалайзера 
         
     function coverInterval() {
         if (cover.getAttribute('style') == 'background-image: url("//myradio24.com/img/nocover.jpg");') {
             cover.setAttribute('style', 'background-image: url(https://vinilavanila.online/wp-content/uploads/2021/05/logo_cover_300.png);');
         }
     }   
-    setInterval(coverInterval, 1);
+    setInterval(coverInterval, 1); // таймер изменения обложки
 
-    audio.volume = 0.5;
-    audio.setAttribute('crossorigin', "anonymous");
+    audio.volume = 0.5; // дефолтная громкость для аудио
+    audio.setAttribute('crossorigin', "anonymous"); // НЕ МЕНЯТЬ!!!
 
-    function changeIcon() {
+    function changeIcon() { // функция для изменения иконки кнопки плэй
         if (audio.paused) {
             playBtn.classList.remove('lightPlayOn');
             playBtn.classList.add('lightPlayOff');
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function changeVolumeIcon(value, icon) {
+    function changeVolumeIcon(value, icon) { // измнение иконки громкости, в зависимости от громкости
 
         if (value == 0) {
             icon.setAttribute('src', 'https://vinilavanila.online/wp-content/uploads/2021/05/volume_icon_off.png');
@@ -74,7 +74,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function checkTimer(item) {
+    function checkTimer(item) { // добавление 0 для цифр в таймере
         if (item <= 9) {
             return '0' + item;
         } else {
@@ -82,7 +82,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function timer() {
+    function timer() { // функция таймера
         seconds++;
         if (seconds == 59) {
             minutes++;
@@ -97,7 +97,7 @@ window.addEventListener('DOMContentLoaded', () => {
         durHours.innerHTML = checkTimer(hours);
     }
 
-    function closeEq(boll = true) {
+    function closeEq(boll = true) { // функция по закрытию/открытию эквалайзера, true - закрыть, false - открыть
         if (boll) {
             radiator.classList.remove('lightLampOff');
             screen.classList.remove('lightScreenOff');
@@ -112,7 +112,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function playAudio() {
+    function playAudio() { // функция по запуску аудир - объеденяет измненения иконок, анимашку
         durTimer = setInterval(timer, 1000);
         screen.style.opacity = '1';
         screen.classList.remove('lightScreenOff');
@@ -137,14 +137,6 @@ window.addEventListener('DOMContentLoaded', () => {
         changeIcon();
     }
 
-    const reg = /@/i;
-
-    function regInterval() {
-        if (reg.test(nextSongText)) {
-            nextSongText.innerHTML = ''
-        }
-    }
-    setInterval(regInterval, 1000);
 
     playBtn.addEventListener('click', () => {
         if (playBtn.getAttribute('data-play') == 0) {
@@ -173,7 +165,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (audio.volume == 0 ) {
             volumeBar.value = volumeStat;
             audio.volume = volumeStat / 100;
-            volumeImg.setAttribute('src', 'https://vinilavanila.online/wp-content/uploads/2021/05/volume_icon_half.png');
+            volumeImg.setAttribute('src', 'https://vinilavanila.online/wp-content/uploads/2021/05/volume_icon_half.png'); // подключать картини лучше напрямую с сервака, иначе потом будет боль менять
         } else {
             volumeStat = volumeBar.value;
             volumeBar.value = 0;
@@ -186,11 +178,11 @@ window.addEventListener('DOMContentLoaded', () => {
         closeEq();
     });
 
-    qualityBtn.addEventListener('click', () => {
+    qualityBtn.addEventListener('click', () => { // изменения качества потока
         if (qualityBtn.getAttribute('data-quality') == 320) {
             qualityBtn.setAttribute('src', 'https://vinilavanila.online/wp-content/uploads/2021/05/bitrate_left.png');
             audio.setAttribute('src', 'https://myradio24.org/vinila_128');
-            if (playBtn.getAttribute('data-play') == 1) {
+            if (playBtn.getAttribute('data-play') == 1) {  // если это убрать, то при нажатии на изменения потока, аудио будет автоматом включаться
                 audio.play();
             }
             qualityBtn.setAttribute('data-quality', 128);
@@ -214,7 +206,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    document.addEventListener('click', (e) => {
+    document.addEventListener('click', (e) => { // тут проверятеся, если кликнули на любую часть страницы, вне списка пресетов - список закрывается
         const target = e.target;
 
         if (target != eqWrap && target != eqWrap.childNodes && target != eqPatBtn) {
@@ -250,11 +242,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // eq 
 
-    const audioCtx = window.AudioContext || window.webkitAudioContext,
+    const audioCtx = window.AudioContext || window.webkitAudioContext, // контекст для кроссбарузерности
           context = new audioCtx();
 
     const source = context.createMediaElementSource(audio),
-          filters = createFilters();
+          filters = createFilters(); // ГЛОБАЛЬНАЯ ПЕРЕМЕННАЯ
 
     let eqObject = {
         filters: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -275,7 +267,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function createFilters() {
-        const frequencies = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 14000];
+        const frequencies = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 14000]; // частоты, по которым будет меняться эквалайзер
         let filters;
 
         filters = frequencies.map(createFilter);
@@ -290,8 +282,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function equalize(audio) {
       
-        audio.onplay = (e)=>{context.resume();}
-        audio.addEventListener('play',() => context.resume());
+        audio.onplay = (e)=>{context.resume();} // ВАЖНАЯ СТРОКА
+        audio.addEventListener('play',() => context.resume()); // ВАЖНАЯ СТРОКА
 
         source.connect(filters[0]);
         filters[filters.length - 1].connect(context.destination);
@@ -318,7 +310,7 @@ window.addEventListener('DOMContentLoaded', () => {
         eqObject.namePattern = eqPatBtn.textContent;
     }
 
-    function getEqSettings() {
+    function getEqSettings() { // нужна для того, чтобы сохранить настройки плеера
         filters.forEach((item, n) => {
             item.gain.value = eqObject.filters[n];
         });
@@ -332,7 +324,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // patterns
 
-    const patterns = {
+    const patterns = { // ГЛОБАЛЬНАЯ ПЕРЕМЕННАЯ
         'rock': [5.4, 4.5, -3.6, -6.6, -2.7, 2.1, 6, 7.5, 7.8, 8.1],
         'jazz': [3, 6.3, 3.6, -3.9, -5.1, 1.2, 9, 1.4, 2, 2.5],
         'dub': [5.1, 4.8, 4.1, 1.5, -2.4, -3.2, -1.6, 1.6, 5.2, 5.5],
@@ -381,7 +373,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }); 
 
     eqPatterns.forEach(item => {
-        item.addEventListener('mouseover', () => {
+        item.addEventListener('mouseover', () => { // нужно для того, чтобы делать ховер для пресетов, костыль
             item.classList.add('player__eq-select-item_before');
         });
         item.addEventListener('mouseout', () => {
@@ -397,13 +389,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // balance
 
-    const stereoPanner = new StereoPannerNode(context);
+    const stereoPanner = new StereoPannerNode(context); // ГЛОБАЛЬНАЯ ПЕРЕМЕННАЯ, нужна для измнения баланса на уши
 
-    Draggable.create(knob, {
-        type: 'rotation',
-        force3D: false,
+    Draggable.create(knob, { // с помощью него knob крутиться
+        type: 'rotation', //  ВАЖНО
+        force3D: false, // для оптимизации
         bounds: {maxRotation: 150, minRotation: -150},
-        onDrag: function() {
+        onDrag: function() { // измнение баланса при изменениии knob
             const rotation = parseInt(this.rotation % 360, 10);
             
             stereoPanner.pan.value = (rotation / 15) / 10;
@@ -423,7 +415,7 @@ window.addEventListener('DOMContentLoaded', () => {
     source.connect(stereoPanner).connect(context.destination);
     // time
 
-    const moskowUtc = 3;
+    const moskowUtc = 3; // часы
     const timerId = setInterval(() => {
         let t = new Date(),
             tt = t.getUTCHours() + moskowUtc;
